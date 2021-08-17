fa8cb4e8f89769dae7fc8b4e66eb25fc7cab0aa8
stefrie <sriehle@gmail.com>
2021-08-17 00:18:18 -0700

    add code

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+
+
+function Footer() {
+    <h3>Author: Stefanie Riehle</h3>
+}
+
+
+
+
+export default Footer;
---
+++ src/Main.js
@@ -0,0 +1,14 @@
+
+function Main() {
+    return (
+        <>
+            <h1>{props.title}</h1>
+            <h2>{props.imageUrl}</h2>
+            <h2>{props.description}</h2>
+        </>
+    );
+}
+
+
+
+export default Footer;
