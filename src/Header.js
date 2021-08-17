ebb8d3283fd61b60cda0c25075244911a84f630c
stefrie <sriehle@gmail.com>
2021-08-17 13:53:33 -0700

    code sync

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,9 @@
 
-
-
 function Header(props) {
     return <header><h1>Title Here: {props.title}</h1></header>;
 }
 
 export default Header;
-
-
 
 // --------------Lab 02------------//
 // function Header() {
