06bffadb310a844f688e53b503a80b8dca62cc5f
stefrie <sriehle@gmail.com>
2021-08-18 17:13:54 -0700

    finished feature 3! also updated README.md

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ import Main from './Main';
 import Footer from './Footer';
 import React from 'react';
 import beastImages from './data.json';
-// import Container from 'react-bootstrap/Container'
-// import Row from 'react-bootstrap/Row'
-// import Col from 'react-bootstrap/Col'
 
 class App extends React.Component {
 	constructor(props) {
